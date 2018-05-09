d0b32a06b20ba107e2f245cb31ba4993331acf09
Chase Smith <mcsmith01@alumni.amherst.edu>
2018-05-09 07:43:35 -0400

    Added problems and tests. Still need a problem 1

---
+++ Homework2.java
@@ -0,0 +1,39 @@
+public class Homework2 {
+
+	
+
+	/* Use String methods to turn the provided String (input) into all uppercase
+	 * letters and return the result
+	 * Hint: The String class has a 'toUpperCase' method
+	 */
+	public static String problem2(String input) {
+
+	}
+
+	/* Use String methods to remove punctuation from the provided String (input)
+	 * Hint: The String class has a 'replace' method
+	 */
+	public static String problem3(String input) {
+
+	}
+
+	/* Fix the errors in the following method
+	 */
+	public static Rectangle problem4() {
+		Rectangle r1 = (5, 10, 15, 20);
+		double width = Rectangle(5, 10, 15, 20).getWidth();
+		Rectangle r2;
+		r2.translate(15, 25);
+		r3 = new Rectangle();
+		r3.translate("Far, far away");
+		return r3;
+	}
+
+	/* Use Rectangle methods to calculate and return the perimiter of the
+	 * provided Rectangle (rect)
+	 */
+	public static int problem5(Rectangle rect) {
+
+	}
+
+}
