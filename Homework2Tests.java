fef5d97ef6de291514737faa38d27b0b13dcd035
Chase Smith <mcsmith01@alumni.amherst.edu>
2018-05-10 07:39:39 -0400

    Rewrote tests

--- Homework2Tests.java
+++ Homework2Tests.java
@@ -26,14 +26,14 @@ public class Homework2Tests {
 			exitCode += 1;
 		}
 
-		if (Homework1.problem4() != null) {
+		if (hw2.problem4() != null) {
 			System.out.println("Pass 4");
 		} else {
 			System.out.println("Fail 4");
 			exitCode += 1;
 		}
 
-		if (Homework1.problem5(new Rectangle(5, 10, 20, 30)) == 100) {
+		if (hw2.problem5(new Rectangle(5, 10, 20, 30)) == 100) {
 			System.out.println("Pass 5");
 		} else {
 			System.out.println("Fail 5");
